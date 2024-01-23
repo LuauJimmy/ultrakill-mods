2dc542ce6c88bcf81fea21dced57e6612b221ad9
Jimmy Caldrone <luaujimmy@gmail.com>
2024-01-23 17:06:05 -0600

    fixed tracked files

--- RcHud/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace EffectChanger
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "EffectChanger";
-        public const string PLUGIN_NAME = "EffectChanger";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
